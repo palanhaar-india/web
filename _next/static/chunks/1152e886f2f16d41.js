cb0da26b90f6c717ce1af2c9e8eb0939eb4e8512
palanhaar-india <161629515+palanhaar-india@users.noreply.github.com>
2025-09-23 22:35:58 +0530

    Add files via upload

---
+++ _next/static/chunks/1152e886f2f16d41.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/614045b6bd551ed4.js",
+  "static/chunks/turbopack-a3356bdfde71eb0e.js"
+])
---
+++ _next/static/chunks/9606ecb41ef0721c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/614045b6bd551ed4.js",
+  "static/chunks/turbopack-02ce48ae5d3f9027.js"
+])
