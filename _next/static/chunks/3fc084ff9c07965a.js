e51826ba4889896124dc46493c738a8c80e33252
Bhagirath Singh <bhagirathsingh@Bhagiraths-MacBook-Pro.local>
2025-09-27 22:01:43 +0530

    GA Fixes

--- _next/static/chunks/3fc084ff9c07965a.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/852ddc3a47457bd5.js",
-  "static/chunks/turbopack-41fa4d20fc573221.js"
-])
--- _next/static/chunks/5ecb94b7cd8e62d3.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/ac32955dfd6ce29f.js",
-  "static/chunks/turbopack-d227cf7eae0525de.js"
-])
